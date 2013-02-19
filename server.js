b9bda293a4a41dfa9840f1bdf05a41860e4af5e7
Patrick D. Kelly <e@pdk.to>
2013-02-18 21:29:27 -1000

    include a bunch of stuff based on express example

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 var settings = require('./settings');
 
 var express = require('express');
-var connect = require('connect');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
 
 var app = express();
 
-// We're going to run this behind nginx in production.
-app.enable('trust proxy');
+app.configure(function(){
+    // We're going to run this behind nginx in production.
+    app.enable('trust proxy');
+    app.set('port', 8888);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser(settings.session_secret));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(require('less-middleware')({ src: __dirname + '/www' }));
+    app.use(express.static(path.join(__dirname, 'www')));
+});
 
-app.use(connect.cookieParser);
-app.use(connect.cookieSession({ secret: settings.session_secret }));
-app.use(express.static('www'));
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
 
-app.listen(8888);
-console.log('Listening on port 8888');
+app.get('/hello.txt', function(req, res){
+    res.send('Hello World!');
+});
+
+app.get('/home', routes.index);
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+});
